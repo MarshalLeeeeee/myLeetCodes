67c6efe3449c2bcad807b6e994d937ed45563038
MarshalLeeeeee <marshallee413lmc@sina.com>
2018-11-28 21:43:49 +0800

    describe problem 5

--- 5-longestPalindrome.py
+++ 5-longestPalindrome.py
@@ -1,6 +1,20 @@
-# O(n^2)
-# similar to approach 4
+'''
+5. Longest Palindromic Substring
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+Example 1:
+Input: "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+
+Example 2:
+Input: "cbbd"
+Output: "bb"
+'''
+
 class Solution:
+    # O(n^2)
+    # similar to approach 4
     def longestPalindrome(self, s):
         """
         :type s: str
