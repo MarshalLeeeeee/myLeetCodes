7787127fdefaf9ec38b6750e6b5eb5519954c020
MarshalLeeeeee <marshallee413lmc@sina.com>
2018-11-30 12:40:38 +0800

    problem 15

---
+++ 15-threeSum.py
@@ -0,0 +1,92 @@
+class Solution:  
+    def ifIn(self,nums,target):
+        left = 0
+        right = len(nums) - 1
+        while(left <= right):
+            mid = (left + right) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                return True
+        return False
+
+    def pairIterator(self,seq):
+        l = len(seq)
+        for i in range(l):
+            for j in range(i+1,l):
+                yield (seq[i],seq[j])
+    
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        length = len(nums)
+        if length < 3:
+            return []
+        if length == 3:
+            return [nums] if nums[0]+nums[1]+nums[2] == 0 else []
+        nums.sort()
+        ans = []
+        
+        duplicate = {}
+        positiveEven = {}
+        negativeEven = {}
+        Odd = {}
+        i = 0
+        while i < length:
+            while(i < length - 1 and nums[i] == nums[i+1]):
+                try:
+                    duplicate[nums[i]] += 1
+                except:
+                    duplicate[nums[i]] = 2
+                i += 1
+            if nums[i] % 2 == 1:
+                Odd[nums[i]] = 1
+            elif nums[i] >= 0 and nums[i] % 2 == 0:
+                positiveEven[nums[i]] = 1
+            else:
+                negativeEven[nums[i]] = 1
+            i += 1
+        for d in duplicate:
+            if d != 0:
+                if self.ifIn(nums,-2*d):
+                    ans.append([d,d,-2*d])
+            else:
+                if duplicate[d] >= 3:
+                    ans.append([0,0,0])
+        OddList = list(Odd.keys())
+        positiveEvenList = list(positiveEven.keys())
+        negativeEvenList = list(negativeEven.keys())
+        for x,y in self.pairIterator(positiveEvenList):
+            try:
+                negativeEven[-(x+y)]
+                ans.append([x,y,-(x+y)])
+            except:
+                continue
+        for x,y in self.pairIterator(negativeEvenList):
+            try:
+                positiveEven[-(x+y)]
+                ans.append([x,y,-(x+y)])
+            except:
+                continue
+        for x,y in self.pairIterator(OddList):
+            if (x+y > 0):
+                try:
+                    negativeEven[-(x+y)]
+                    ans.append([x,y,-(x+y)])
+                except:
+                    continue
+            else:
+                try:
+                    positiveEven[-(x+y)]
+                    ans.append([x,y,-(x+y)])
+                except:
+                    continue
+        return ans
+        
+if __name__ == '__main__':
+    #print(Solution().threeSum([-6,-6,-4,-2,0,1,3,3,5,6,6]))
+    print(Solution().threeSum([-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0]))
