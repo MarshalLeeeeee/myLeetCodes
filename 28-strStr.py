780dd1dc5f1531315ac8125dfbfbcfb39ac81705
MarshalLeeeeee <marshallee413lmc@sina.com>
2018-12-03 17:36:23 +0800

    solve with KMP

--- 28-strStr.py
+++ 28-strStr.py
@@ -34,7 +34,48 @@ class Solution:
                 return i
         return -1
 
-
+class Solution2:
+    # try kmp
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if not needle:
+            return 0
+        if not haystack:
+            return -1
+        length = len(needle)
+        kmp = [0]
+        prefixTail = 0
+        i = 1
+        while i < length:
+            if needle[i] == needle[prefixTail]:
+                prefixTail += 1
+                kmp.append(prefixTail)    
+                i += 1
+            else:
+                if prefixTail == 0:
+                    kmp.append(0)
+                    i += 1
+                else:
+                    prefixTail -= prefixTail - kmp[prefixTail-1]
+        print(kmp)
+        prefixTail = 0
+        i = 0
+        while i < len(haystack):
+            if haystack[i] == needle[prefixTail]:
+                prefixTail += 1
+                if prefixTail == length:
+                    return i-length+1
+                i += 1
+            else:
+                if not prefixTail:
+                    i += 1
+                else:
+                    prefixTail -= prefixTail - kmp[prefixTail-1]
+        return -1
 
 if __name__ == '__main__':
     print(Solution().strStr("ababcaababcaabc","ababcaabc"))
