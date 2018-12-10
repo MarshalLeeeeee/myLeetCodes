067cad463e19f5aee649656f437930f903ddb866
MarshalLeeeeee <marshallee413lmc@sina.com>
2018-12-10 12:01:09 +0800

    problem 49

---
+++ 49-groupAnagrams.py
@@ -0,0 +1,46 @@
+'''
+49. Group Anagrams
+
+Given an array of strings, group anagrams together.
+
+Example:
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Output:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+Note:
+
+All inputs will be in lowercase.
+The order of your output does not matter.
+'''
+
+class Solution:
+    # merely below the time limits
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        dicts = []
+        ans = []
+        for word in strs:
+            letterDict = {}
+            for w in word:
+                try:
+                    letterDict[w] += 1
+                except:
+                    letterDict[w] = 1
+            if letterDict in dicts:
+                for i,d in enumerate(dicts):
+                    if d == letterDict:
+                        ans[i].append(word)
+            else:
+                dicts.append(letterDict)
+                ans.append([word])
+        return ans
+
+if __name__ == '__main__':
+    print(Solution().groupAnagrams(strs))
