4dd95ebde5934c263791a176fa65d1d5870ac56b
MarshalLeeeeee <marshallee413lmc@sina.com>
2018-12-10 11:09:41 +0800

    add intro

--- 47-permuteUnique.py
+++ 47-permuteUnique.py
@@ -1,3 +1,18 @@
+'''
+47. Permutations II
+
+Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+
+Example:
+Input: [1,1,2]
+Output:
+[
+  [1,1,2],
+  [1,2,1],
+  [2,1,1]
+]
+'''
+
 class Solution:
     # only work for positive integers
     def permuteNum(self,nums,length):
