6212a793e65331e5e31d65eccb9fc14469fd3a1c
MarshalLeeeeee <marshallee413lmc@sina.com>
2018-12-18 13:30:39 +0800

    iteration solution

--- 94-binaryTreeInorderTraversal.py
+++ 94-binaryTreeInorderTraversal.py
@@ -17,3 +17,24 @@ class Solution:
             left = self.inorderTraversal(root.left)
             right = self.inorderTraversal(root.right)
             return left+[root.val]+right
+
+class Solution2:
+    # iteration
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root: return []
+        else:
+            stack, res = [root], []
+            while stack:
+                if stack[-1]:
+                    stack.append(stack[-1].left)
+                else:
+                    stack.pop()
+                    if stack:
+                        res.append(stack[-1].val)
+                        x = stack.pop()
+                        stack.append(x.right)
+            return res
