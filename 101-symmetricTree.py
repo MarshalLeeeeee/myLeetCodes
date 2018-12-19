2dfaa3300d56dc6b46ac7befa5dcf25262048f28
MarshalLeeeeee <marshallee413lmc@sina.com>
2018-12-19 19:25:39 +0800

    iteration solution

--- 101-symmetricTree.py
+++ 101-symmetricTree.py
@@ -23,4 +23,25 @@ class Solution:
             elif not root.left and not root.right: return True
             else: return False
                 
-        
+class Solution2:
+    # iteratively solution
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root: return True
+        else:
+            stack = [(root,root)]
+            while stack:
+                try:
+                    if stack[-1][0] and stack[-1][1]: stack.append((stack[-1][0].left,stack[-1][1].right))
+                    elif not stack[-1][0] and not stack[-1][1]:
+                        stack.pop()
+                        if stack:
+                            left, right = stack.pop()
+                            if left.val != right.val: return False
+                            stack.append((left.right, right.left))
+                    else: return False
+                except: return False
+            return True
