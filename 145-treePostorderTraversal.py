19edb6d7ae68e3f298654866998932059db05fe8
MarshalLeeeeee <marshallee413lmc@sina.com>
2018-12-26 16:10:03 +0800

    iteration with structure modified

---
+++ 145-treePostorderTraversal.py
@@ -0,0 +1,44 @@
+'''
+144. Binary Tree Preorder Traversal
+
+Given a binary tree, return the preorder traversal of its nodes' values.
+
+Example:
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,2,3]
+Follow up: Recursive solution is trivial, could you do it iteratively?
+'''
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    # iterative solution
+    # the structure is broken
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root: return []
+        res, stack = [], [root]
+        while stack:
+            node = stack[-1]
+            if not node.left and not node.right: 
+                res.append(node.val)
+                stack.pop()
+            else:
+                if node.right: stack.append(node.right); node.right = None
+                if node.left: stack.append(node.left); node.left = None
+        return res
+        
