3f4c7f0346554bdad8342ce2e2f6a796313bcdd5
MarshalLeeeeee <marshallee413lmc@sina.com>
2019-01-29 09:16:14 +0800

    dfs solution

---
+++ 279-perfectSquare.py
@@ -0,0 +1,47 @@
+'''
+279. Perfect Squares
+
+Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+
+Example 1:
+Input: n = 12
+Output: 3 
+Explanation: 12 = 4 + 4 + 4.
+
+Example 2:
+Input: n = 13
+Output: 2
+Explanation: 13 = 4 + 9.
+'''
+
+class Solution:
+    # dictionary version
+    # recursion takes time
+    def dfs(self,n,d):
+        if n in d: return d[n]
+        i = 1
+        while i*i < n:
+            self.dfs(n-i*i,d)
+            if n in d: d[n] = min(d[n],d[n-i*i]+1)
+            else: d[n] = d[n-i*i]+1
+            i += 1
+        return d[n]
+        
+    def numSquares(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        d, i = {}, 1
+        while i*i < n:
+            d[i*i] = 1
+            i += 1
+        if i*i == n: return 1        
+        self.dfs(n,d)
+        print(len(d))
+        return d[n]
+
+
+
+if __name__ == '__main__':
+    print(Solution().numSquares(356))
